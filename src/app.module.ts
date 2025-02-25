a0289f0327b5cc8ef65144309f8f8e7528d93a02
Bruno Barreiras <brunooliver368@gmail.com>
2025-02-25 18:38:55 -0300

    fix: adjust env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
